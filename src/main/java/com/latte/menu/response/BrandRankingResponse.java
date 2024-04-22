f96f585aac55f920b6fdb2dd42f8841d3ef55657
HwaJong <hwajong1016@gmail.com>
2024-04-22 16:22:34 +0900

    feat: 브랜드별 랭킹, 카테고리, 검색 구현
    카페 브랜드별 랭킹의 인기순 정렬은 회원 개발 후에 진행 가능

---
+++ src/main/java/com/latte/menu/response/BrandRankingResponse.java
@@ -0,0 +1,12 @@
+package com.latte.menu.response;
+
+import lombok.Data;
+
+@Data
+public class BrandRankingResponse {
+    private Long menuNo;
+    private String menuName;
+    private String brand;
+    private String caffeine;
+    private String imageUrl;
+}
