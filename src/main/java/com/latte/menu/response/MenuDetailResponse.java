e7893ddf723df381d5192504b80c75ce94bd33e2
HwaJong <hwajong1016@gmail.com>
2024-05-03 23:35:52 +0900

    fix: 메뉴 상세 조회 API 확정

--- src/main/java/com/latte/menu/response/MenuDetailResponse.java
+++ src/main/java/com/latte/menu/response/MenuDetailResponse.java
@@ -17,6 +17,7 @@ public class MenuDetailResponse {
     private Level level;
     private String imageUrl;
     private List<MenuSimpleResponse> lowCaffeineMenus;
+    private List<String> otherSizes;
 
     @Data
     public static class Nutrient {
