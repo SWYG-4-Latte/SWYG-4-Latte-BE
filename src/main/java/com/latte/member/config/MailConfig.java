07a0fa13c9b4b006c1dfda9af4e7992ec17d2a94
yeonjikim <yeondus885@aiicon.kr>
2024-05-07 18:30:56 +0900

    java mail

--- src/main/java/com/latte/member/config/MailConfig.java
+++ src/main/java/com/latte/member/config/MailConfig.java
@@ -1,23 +1,28 @@
 package com.latte.member.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@ComponentScan
 @Configuration
 public class MailConfig {
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        javaMailSender.setHost("smtp.naver.com");
-        javaMailSender.setUsername("duswlskfk42@naver.com");
-        javaMailSender.setPassword("passwod!");
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587); // Naver SMTP 포트 번호
+        javaMailSender.setUsername("illywilly2750@gmail.com");
+        javaMailSender.setPassword("falcon2453!");
 
-        javaMailSender.setPort(465);
+
+        //javaMailSender.setPort(465);
+        getMailProperties();
 
         javaMailSender.setJavaMailProperties(getMailProperties());
 
@@ -30,8 +35,10 @@ public class MailConfig {
         properties.setProperty("mail.smtp.auth", "true");
         properties.setProperty("mail.smtp.starttls.enable", "true");
         properties.setProperty("mail.debug", "true");
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
-        properties.setProperty("mail.smtp.ssl.enable","true");
+        properties.setProperty("mail.imap.auth.login.disable", "true");
+        properties.setProperty("mail.imap.auth.plain.disable", "true");
+        //properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        //properties.setProperty("mail.smtp.ssl.enable","false");
         return properties;
     }
 }
