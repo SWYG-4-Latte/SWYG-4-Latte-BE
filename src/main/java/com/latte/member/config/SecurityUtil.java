46810bfa7b176331e264dd6c6505ea84a693b6e9
yeonjikimro <duswlskfk42@naver.com>
2024-06-25 21:34:34 +0900

    회원수정 토큰 오류 수정

--- src/main/java/com/latte/member/config/SecurityUtil.java
+++ src/main/java/com/latte/member/config/SecurityUtil.java
@@ -3,16 +3,42 @@ package com.latte.member.config;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 // 어떤 회원이 API를 요청했는지 조회(test용)
 public class SecurityUtil {
 
     public static String getCurrentUsername() {
+
+
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication == null || authentication.getName() == null) {
             throw new RuntimeException("No authentication information");
         }
         User user = (User) authentication.getPrincipal();
-        return user.getUsername();
+        //return user.getUsername();
+
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else if (principal instanceof String) {
+            return (String) principal;
+        } else {
+            throw new RuntimeException("Unknown principal type");
+        }
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
